c90ebed4f6c3fda4fed970348e24d7a5189d37e0
변수 연 <xjkooy100@gmail.com>
2026-02-22 00:47:13 +0900

    Title: 특정 수 맞히기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260222/특정 수 맞히기/catching-specific-number.js
+++ 260222/특정 수 맞히기/catching-specific-number.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split('\n');
 
-for(let i=0; i<input.length; i++){
-    if(input[i]<25) console.log('Higher');
-    else if(input[i]>25) console.log('Lower');
-    else console.log('Good');
+for (let i = 0; i < input.length; i++) {
+    if (input[i] < 25) console.log('Higher');
+    else if (input[i] > 25) console.log('Lower');
+    else { 
+        console.log('Good'); 
+        break; 
+    }
 }

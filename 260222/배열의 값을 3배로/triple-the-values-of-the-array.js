4fc6c4ff834b607841293edb541d0500f5a66234
변수 연 <xjkooy100@gmail.com>
2026-02-22 01:07:22 +0900

    Title: 배열의 값을 3배로, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260222/배열의 값을 3배로/triple-the-values-of-the-array.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const arr = input.map(input => input.split(' ').map(Number));
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    arr[i][j] *= 3;
+  }
+}
+
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i].join(' '));
+}

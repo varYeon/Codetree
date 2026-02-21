d011438538cfa48a1de6ce94bda06349b294e149
변수 연 <xjkooy100@gmail.com>
2026-02-21 23:37:57 +0900

    Title: 짝수의 합 n번 구하기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
+++ 260221/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.js
@@ -17,3 +17,19 @@ for (let i = 1; i <= n; i++) {
     console.log(sum);
 }
 
+// 이전 풀이 
+/*
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(/\s+/);
+
+for (let i = 1; i < input.length; i += 2) {
+    let a = Number(input[i]);
+    let b = Number(input[i + 1]);
+    let sum = 0;
+
+    for (let j = a; j <= b; j++) {
+        if (j % 2 === 0) sum += j;
+    }
+    console.log(sum);
+}
+*/

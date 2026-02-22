61cad934db7154a61e700f76b4133670a534fe19
변수 연 <xjkooy100@gmail.com>
2026-02-22 13:30:43 +0900

    Title: 두 배열의 곱, Time: 72ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260222/두 배열의 곱/multiple-of-two-arrays.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n\n');
+const arr1 = input[0].split('\n');
+const arr2 = input[1].split('\n');
+
+// console.log(arr1); //[ '1 2 3', '4 5 6', '7 8 9' ]
+// console.log(arr2); //[ '2 3 4', '5 6 7', '8 9 10' ]
+
+const arr_multi = [];
+
+for (let i = 0; i < arr1.length; i++) {
+    const arr1_num = arr1[i].split(' ').map(Number);
+    const arr2_num = arr2[i].split(' ').map(Number);
+    // console.log(arr1_num); //[ 1, 2, 3 ]
+    // console.log(arr2_num); //[ 2, 3, 4 ]
+
+    for (let j = 0; j < arr1_num.length; j++) {
+        arr_multi.push(arr1_num[j] * arr2_num[j]);
+    }
+    // console.log(arr_multi); //[ 2, 6, 12 ... ]
+}
+
+const result = [];
+const n = 3;
+
+for (let i = 0; i < n; i++) {
+    const row = [];
+    for (let j = 0; j < n; j++) {
+        row.push(arr_multi[i * n + j]);
+    }
+    result.push(row);
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(result[i].join(' '));
+}

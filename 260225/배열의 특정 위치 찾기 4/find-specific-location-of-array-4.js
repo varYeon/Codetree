83e4406c472ab05b1ab72db30857b966f6aa7877
변수 연 <xjkooy100@gmail.com>
2026-02-25 21:31:45 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260225/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+let arr = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let sum = 0;
+let count = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) break;
+    else {
+        if (arr[i] % 2 === 0) {
+            sum += arr[i];
+            count++;
+        }
+    }
+}
+
+console.log(count, sum);

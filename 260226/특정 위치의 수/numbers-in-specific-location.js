265decb495a282adb2da949e77fa9f2494f95472
변수 연 <xjkooy100@gmail.com>
2026-02-26 17:05:38 +0900

    Title: 특정 위치의 수, Time: 65ms, Memory: 11MB, Status: Passed - Codetree

--- 260226/특정 위치의 수/numbers-in-specific-location.js
+++ 260226/특정 위치의 수/numbers-in-specific-location.js
@@ -1,10 +1,4 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-let sum = 0;
-
-for (let i = 0; i < input.length; i++) {
-    if (i === 2 || i === 4 || i === 9) sum += input[i];
-}
-
-console.log(sum);
+console.log(input[2]+input[4]+input[9]);

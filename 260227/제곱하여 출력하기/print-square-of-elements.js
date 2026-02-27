7f6a1e38ae082b00d091ed2fa9a02d594a47d2c2
변수 연 <xjkooy100@gmail.com>
2026-02-27 14:40:50 +0900

    Title: 제곱하여 출력하기, Time: 74ms, Memory: 36MB, Status: Passed - Codetree

---
+++ 260227/제곱하여 출력하기/print-square-of-elements.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number)
+
+for (let i=0; i<n; i++) {
+    arr[i] *= arr[i];
+}
+
+console.log(arr.join(' '));

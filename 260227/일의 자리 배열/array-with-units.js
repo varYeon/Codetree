8d9cbf60017dfced548bd6c48fd6a05fbb9ae2c0
변수 연 <xjkooy100@gmail.com>
2026-02-27 15:03:20 +0900

    Title: 일의 자리 배열, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260227/일의 자리 배열/array-with-units.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ');
+
+const first = Number(input[0]);
+const second = Number(input[1]);
+const arr = [];
+arr.push(first, second);
+
+for (let i = 2; i <= 9; i++) {
+    let a = arr[i - 2] + arr[i - 1];
+    if (a >= 10) a %= 10;
+
+    arr.push(a);
+}
+
+console.log(arr.join(' '));

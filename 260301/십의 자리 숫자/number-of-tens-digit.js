0f33fd5b596f4965b0035c26c0f671f8846193ee
변수 연 <xjkooy100@gmail.com>
2026-03-01 15:20:36 +0900

    Title: 십의 자리 숫자, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/십의 자리 숫자/number-of-tens-digit.js
+++ 260301/십의 자리 숫자/number-of-tens-digit.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
-let numbers = fs.readFileSync(0).toString().trim().split(" ");
+let numbers = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 for (let i = 1; i <= 9; i++) {
     let count = 0;
     for (let j = 0; j < numbers.length; j++) {
+        if (numbers[j] === 0) break;
+        if (numbers[j] < 10) continue;
+
         const ten = Math.floor(numbers[j] / 10);
-        if (ten == i) count++;
+        if (ten === i) count++;
     }
     console.log(`${i} - ${count}`);
 }

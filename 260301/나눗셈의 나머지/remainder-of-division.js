f475fb721dfdfc55053649768839a33342b66ea9
변수 연 <xjkooy100@gmail.com>
2026-03-01 18:06:26 +0900

    Title: 나눗셈의 나머지, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260301/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let a = input[0];
+const b = input[1];
+let remain = [];
+
+while (a > 1) {
+    remain.push(Math.floor(a % b));
+    console.log(a % b)
+    a = Math.floor(a / b);
+}
+
+let result = 0;
+
+for (let i = 0; i < b; i++) {
+    let count = 0;
+    for (let j = 0; j < remain.length; j++) {
+        if (remain[j] === i) count++;
+    }
+    result += count ** 2;
+}
+
+console.log(result);

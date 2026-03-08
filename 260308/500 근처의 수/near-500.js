693c5d35ffd89a2f60ff74091c73f165824933a3
변수 연 <xjkooy100@gmail.com>
2026-03-08 20:55:42 +0900

    Title: 500 근처의 수, Time: 65ms, Memory: 12MB, Status: Passed - Codetree

--- 260308/500 근처의 수/near-500.js
+++ 260308/500 근처의 수/near-500.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
 let nums = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-// let big = nums[0]; // nums[0] 의 범위가 500 이상 이라면?
-// let small = nums[1]; // nums[0] 의 범위가 500 이하 라면?
-let big = 0;
-let small = 0;
+let maxUnder = -Infinity;
+let minOver  = Infinity;
 
-for (let i = 0; i < 10; i++) {
-    if (nums[i] < 500) {
-        big = nums[i];
-        continue;
-    }
-    if (nums[i] > 500) {
-        small = nums[i];
-        break;
-    }
+for (let i = 0; i < nums.length; i++) {
+  const x = nums[i];
+
+  if (x < 500 && x > maxUnder) {
+    maxUnder = x;
+  }
+
+  if (x > 500 && x < minOver) {
+    minOver = x;
+  }
 }
-for (let i = 0; i < 10; i++) {
-    if (nums[i] >= big) big = nums[i];
-    if (nums[i] <= small) small = nums[i];
-}
+
+const big = maxUnder;
+const small = minOver;
 
 console.log(big, small);

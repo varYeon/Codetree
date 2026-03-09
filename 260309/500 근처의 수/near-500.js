18f29cc295bb4744f35a0ed9712c14a213163249
변수 연 <xjkooy100@gmail.com>
2026-03-09 18:47:13 +0900

    Title: 500 근처의 수, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260309/500 근처의 수/near-500.js
@@ -0,0 +1,14 @@
+// *
+
+const fs = require('fs');
+let nums = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+let big = 1;
+let small  = 1000;
+
+for (let i = 0; i < 10; i++) {
+  if (nums[i] < 500 && nums[i] > big) big = nums[i];
+  if (nums[i] > 500 && nums[i] < small) small = nums[i];
+}
+
+console.log(big, small);

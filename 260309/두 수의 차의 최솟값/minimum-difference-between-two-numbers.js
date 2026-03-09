2a20199dcd5b73a6094f54ca0c890cb39ab092d0
변수 연 <xjkooy100@gmail.com>
2026-03-09 19:08:25 +0900

    Title: 두 수의 차의 최솟값, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260309/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
+++ 260309/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -6,6 +6,8 @@ const nums = input[1].split(' ').map(Number);
 
 let minDivide = 10000;
 
+// 풀이 1
+/*
 for (let i = 0; i < all; i++) {
     const a = nums[i];
 
@@ -16,6 +18,15 @@ for (let i = 0; i < all; i++) {
 
         if (divide !== 0 && divide <= minDivide) minDivide = divide;
     }
+}
+*/
+
+// 풀이 2
+// 오름차순 정렬, 가장 가까이 있는 수가 가장 작은 차이를 보임
+for (let i = 0; i < all; i++) {
+    const divide = nums[i + 1] - nums[i];
+
+    if (divide !== 0 && divide <= minDivide) minDivide = divide;
 }
 
 console.log(minDivide);

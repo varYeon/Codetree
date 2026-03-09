890257a16f62087ef68053752d3db4a25fa7c5fd
변수 연 <xjkooy100@gmail.com>
2026-03-09 18:58:07 +0900

    Title: 두 수의 차의 최솟값, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260309/두 수의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const all = Number(input[0]);
+const nums = input[1].split(' ').map(Number);
+
+let minDivide = 10000;
+
+for (let i = 0; i < all; i++) {
+    const a = nums[i];
+
+    for (let j = 1; j < all - 1; j++) {
+        const b = nums[j];
+
+        const divide = Math.abs(a - b);
+
+        if (divide !== 0 && divide <= minDivide) minDivide = divide;
+    }
+}
+
+console.log(minDivide);

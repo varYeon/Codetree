5d1466b23145de3ea329fe71775685c30e834f08
변수 연 <xjkooy100@gmail.com>
2026-03-13 16:54:43 +0900

    Title: 격자 반대로 채우기, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260313/격자 반대로 채우기/grid-anti-fill.js
+++ 260313/격자 반대로 채우기/grid-anti-fill.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 let n = Number(fs.readFileSync(0).toString().trim());
 
 const result = [];
-let num = 0;
+let num = 1;
 
 // 배열 초기화
-for (let i = 0; i< n; i++) {
+for (let i = 0; i < n; i++) {
     result[i] = [];
     for (let j = 0; j < n; j++) {
         result[i][j] = 0;
@@ -14,12 +14,14 @@ for (let i = 0; i< n; i++) {
 // 33 23 13 03 02 12 22 32
 // [i] -> 조건부 -> 행
 // [j] -> 하나씩 감소 -> 열
-// 열이 짝수일 때 -> 00 10 20 30, 02 12 22 32
-// 열이 홀수일 때 -> 31 21 11 01, 33 23 13 03
+// 열이 짝수일 때 -> 00 10 20 30, 02 12 22 32 (i가 늘어남, 위->아래)
+// 열이 홀수일 때 -> 31 21 11 01, 33 23 13 03 (i가 줄어듦, 아래->위)
+// 시작점 : 33
 
 // 값 넣기
-for (let j = n-1; j >= 0; j--) {
-    if (j % 2 !== 0) {
+for (let j = n - 1; j >= 0; j--) {
+
+    if (j % 2 === 0) {
         for (let i = 0; i < n; i++) {
             result[i][j] = num;
             num += 1;
@@ -34,4 +36,4 @@ for (let j = n-1; j >= 0; j--) {
 
 for (let i = 0; i < n; i++) {
     console.log(result[i].join(' '));
-}
+} 

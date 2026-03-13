754ba4b20867b2199705895f6e5882ff098fc242
변수 연 <xjkooy100@gmail.com>
2026-03-13 16:34:32 +0900

    Title: 격자 반대로 채우기, Time: 69ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260313/격자 반대로 채우기/grid-anti-fill.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+
+const result = [];
+let num = 0;
+
+// 배열 초기화
+for (let i = 0; i< n; i++) {
+    result[i] = [];
+    for (let j = 0; j < n; j++) {
+        result[i][j] = 0;
+    }
+}
+// 33 23 13 03 02 12 22 32
+// [i] -> 조건부 -> 행
+// [j] -> 하나씩 감소 -> 열
+// 열이 짝수일 때 -> 00 10 20 30, 02 12 22 32
+// 열이 홀수일 때 -> 31 21 11 01, 33 23 13 03
+
+// 값 넣기
+for (let j = 0; j < n; j++) {
+    if (j % 2 === 0) {
+        for (let i = 0; i < n; i++) {
+            result[i][j] = num;
+            num += 1;
+        }
+    } else {
+        for (let i = n - 1; i >= 0; i--) {
+            result[i][j] = num;
+            num += 1;
+        }
+    }
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(result[i].join(' '));
+}

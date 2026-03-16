f3959ccd20ff095a78389f09afcdd21e3c2d0e06
변수 연 <xjkooy100@gmail.com>
2026-03-16 18:08:27 +0900

    Title: 파스칼의 삼각형, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260316/파스칼의 삼각형/pascals-triangle.js
+++ 260316/파스칼의 삼각형/pascals-triangle.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 let n = Number(fs.readFileSync(0).toString().trim());
 
+/* 방법 1
+// 초기화
 const arr2d = [];
 
-// 초기화
 for (let i = 0; i < n; i++) {
     arr2d[i] = [];
     for (let j = 0; j < i + 1; j++) {
@@ -22,6 +23,27 @@ for (let i = 2; i < n; i++) {
             num = arr2d[i - 1][j - 1] + arr2d[i - 1][j];
         }
         arr2d[i][j] = num;
+    }
+}
+
+// 출력
+for (let i = 0; i < n; i++) {
+    console.log(arr2d[i].join(' '));
+}
+*/
+
+// 방법 2
+// 초기화
+const arr2d = Array.from({ length: n }, () => []);
+for (let i = 0; i < n; i++) {
+    arr2d[i][i] = 1;
+    arr2d[i][0] = 1;
+}
+
+// 값
+for (let i = 0; i < n; i++) {
+    for (let j = 1; j < i; j++) {
+        arr2d[i][j] = arr2d[i - 1][j - 1] + arr2d[i - 1][j];
     }
 }
 

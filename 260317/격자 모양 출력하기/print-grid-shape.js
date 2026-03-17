f357c375d3c17e121ec2a5e698b6853c72b2bfe0
변수 연 <xjkooy100@gmail.com>
2026-03-17 17:05:32 +0900

    Title: 격자 모양 출력하기, Time: 68ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 260317/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split('\n');
+
+const nm = input[0].split(' ').map(Number);
+const n = nm[0];
+const m = nm[1];
+
+const arr2d = Array.from({ length: n }, () => new Array(n).fill(0));
+
+for (let i = 1; i <= m; i++) {
+    const rc = input[i].split(' ').map(Number);
+    const r = rc[0];
+    const c = rc[1];
+
+    arr2d[r - 1][c - 1] = c*r;
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(arr2d[i].join(' '));
+}
+

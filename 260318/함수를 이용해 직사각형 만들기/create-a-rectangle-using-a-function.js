c152d35d01e814d899fc50dd389773f86ff08790
변수 연 <xjkooy100@gmail.com>
2026-03-18 20:55:50 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 260318/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
+++ 260318/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -1,3 +1,4 @@
+/*
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 const n = input[0];
@@ -9,4 +10,19 @@ for (let i = 0; i < n; i++) {
         str += '1';
     }
     console.log(str);
+} */
+
+// 함수 사용
+function printOne(n, m) {
+    for (let i = 0; i < n; i++) {
+        let str = ''
+        for (let j = 0; j < m; j++) {
+            str += '1';
+        }
+        console.log(str);
+    }
 }
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+printOne(input[0], input[1]);

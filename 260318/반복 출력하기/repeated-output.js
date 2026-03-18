93b3fb1bad805e6f1822a5a122a2ac49f5ca7c29
변수 연 <xjkooy100@gmail.com>
2026-03-18 20:35:03 +0900

    Title: 반복 출력하기, Time: 77ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260318/반복 출력하기/repeated-output.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+function printString() {
+    console.log('12345^&*()_');
+}
+
+for (let i = 0; i < n; i++) {
+    printString();
+}

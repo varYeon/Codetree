68dae19ef7cd42cd2e5b8226b114240488753850
변수 연 <xjkooy100@gmail.com>
2026-03-25 10:50:15 +0900

    Title: 두 정수 값 교환하기, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260325/두 정수 값 교환하기/to-exchange-two-integer-values.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, m] = input[0].split(" ").map(Number);
+
+
+
+function changePring(n, m) {
+
+}
+
+
+console.log(n, m);

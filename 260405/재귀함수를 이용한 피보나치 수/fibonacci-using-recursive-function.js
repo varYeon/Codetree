d8b95f8e91321a17bbeb3e720e56744af1f3f74e
변수 연 <xjkooy100@gmail.com>
2026-04-05 23:43:30 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+function fibonacci(n) {
+    if (n === 1 || n === 2) return 1;
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(n));

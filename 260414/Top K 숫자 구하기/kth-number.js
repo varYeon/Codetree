597b2e0988034006352f0cd415953e7ba40514b4
변수 연 <xjkooy100@gmail.com>
2026-04-14 19:56:24 +0900

    Title: Top K 숫자 구하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260414/Top K 숫자 구하기/kth-number.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input[1].trim().split(' ').map(Number);
+
+const sortNums = nums.sort();
+console.log(sortNums[k - 1]);

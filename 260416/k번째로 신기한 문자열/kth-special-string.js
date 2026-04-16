75440d58e943592de6e3729cce85fbb75051ab6c
변수 연 <xjkooy100@gmail.com>
2026-04-16 12:47:48 +0900

    Title: k번째로 신기한 문자열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let index = 0;
+const [n, k, t] = input[index++].split(' ');
+const words = [];
+for (let i = 0; i < Number(n); i++) {
+    words.push(input[index++]);
+}
+
+const newWords = words.sort().filter((word)=>word.startsWith(t));
+
+console.log(newWords[k-1]);

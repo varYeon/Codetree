9dca3507793fdc9a7e8709e8faf08b0b80c83eee
변수 연 <xjkooy100@gmail.com>
2026-04-20 16:15:05 +0900

    Title: 개인정보, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/개인정보/personal-info.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const students = [];
+for (let i = 0; i < 5; i++) {
+  const [name, heightStr, weightStr] = input[i].split(' ');
+  const height = Number(heightStr);
+  const weight = Number(weightStr);
+  students.push({ name, height, weight });
+}
+/**
+ [
+  { name: 'lee', height: 167, weight: 40.1 },
+  { name: 'kim', height: 149, weight: 32.9 },
+  { name: 'park', height: 161, weight: 53.1 },
+  { name: 'choi', height: 183, weight: 70.3 },
+  { name: 'jung', height: 155, weight: 45.7 }
+]
+ */
+
+const nameArray = students.sort((a, b) => a.name.localeCompare(b.name)).map(student =>
+  `${student.name} ${student.height} ${student.weight}`
+);
+const heightArray = students.sort((a, b) => b.height - a.height).map(student =>
+  `${student.name} ${student.height} ${student.weight}`
+);;
+
+console.log('name');
+console.log(nameArray.join('\n'));
+console.log('\nheight');
+console.log(heightArray.join('\n'));

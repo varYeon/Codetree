e77bf2dc96a8e7e3fa2713db3e2a77a038da363c
변수 연 <xjkooy100@gmail.com>
2026-04-27 10:56:18 +0900

    Title: 십진수로 변환하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/십진수로 변환하기/convert-to-decimal.js
+++ 260427/십진수로 변환하기/convert-to-decimal.js
@@ -5,7 +5,7 @@ const binaryStr = input[0];
 const binary = binaryStr.split('').map((a) => Number(a));
 let num = 0;
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < binary.length; i++) {
     num = num * 2 + binary[i];
 }
 

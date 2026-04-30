487140409154f1c76322484fe7c1a55d21345962
변수 연 <xjkooy100@gmail.com>
2026-04-30 16:48:51 +0900

    Title: 최대로 겹치는 구간, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 260430/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -4,19 +4,22 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const segments = [];
 for (let i = 1; i <= n; i++) {
-    segments.push(input[i].split(' ').map(Number));
+  segments.push(input[i].split(' ').map(Number));
 } // [ [ 1, 5 ], [ 4, 6 ], [ 3, 5 ] ]
 
 // 좌표 최댓값
-let maxCoord = 0;
+let minCoord = -100000;
+let maxCoord = 100000;
 for (let i = 0; i < segments.length; i++) {
   const s = segments[i][0];
   const e = segments[i][1];
 
+  if (s < minCoord) minCoord = s;
   if (e > maxCoord) maxCoord = e;
 }
+const size = maxCoord - minCoord + 1;
 
-let checked = new Array(maxCoord).fill(0);
+let checked = new Array(size).fill(0);
 
 for (let i = 0; i < n; i++) {
   const start = segments[i][0];
@@ -24,7 +27,8 @@ for (let i = 0; i < n; i++) {
 
   for (let j = start; j < end; j++) {
     // 구간 [start, end), end는 포함x
-    checked[j] += 1;
+    const idx = j - minCoord;
+    checked[idx] += 1;
   }
 }
 

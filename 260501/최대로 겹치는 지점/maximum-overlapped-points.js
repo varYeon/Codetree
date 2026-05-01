62f2a52c9859d5321a3b65984a4620ea050ff53f
변수 연 <xjkooy100@gmail.com>
2026-05-01 14:22:19 +0900

    [Passed] 최대로 겹치는 지점 (65ms, 10MB)

--- 260501/최대로 겹치는 지점/maximum-overlapped-points.js
+++ 260501/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -7,19 +7,7 @@ for (let i = 1; i <= n; i++) {
   segments.push(input[i].split(' ').map(Number));
 }
 
-// 좌표 최대값
-let minCoord = -100000;
-let maxCoord = 100000;
-for (let i = 0; i < segments.length; i++) {
-  const s = segments[i][0];
-  const e = segments[i][1];
-
-  if (s < minCoord) minCoord = s;
-  if (e > maxCoord) maxCoord = e;
-}
-const size = maxCoord - minCoord + 1;
-
-let checked = new Array(size).fill(0);
+let checked = new Array(101).fill(0);
 
 for (let i = 0; i < n; i++) {
   const start = segments[i][0];

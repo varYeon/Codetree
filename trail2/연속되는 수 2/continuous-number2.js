cb04604d9f4f6ba0d5a1836a9067dc7415898fd2
변수 연 <xjkooy100@gmail.com>
2026-05-08 20:36:14 +0900

    [Passed] 연속되는 수 2 (69ms, 14MB)

---
+++ trail2/연속되는 수 2/continuous-number2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+let cnt = 0;
+let maxCnt = 0;
+
+for (let i = 0; i < n; i++) {
+    if (i == 0 || arr[i] !== arr[i - 1]) {
+        cnt = 1;
+    } else {
+        cnt += 1;
+    }
+
+    if (cnt > maxCnt) {
+        maxCnt = cnt;
+    }
+}
+
+console.log(maxCnt);

c8cbac79a81a1015ac1a79701c441b9a6212e6cf
변수 연 <xjkooy100@gmail.com>
2026-05-19 19:28:19 +0900

    [Memory Limit Exceed] 선두를 지켜라 3 (738ms, 160MB)

--- trail2/선두를 지켜라 3/keep-the-lead-3.js
+++ trail2/선두를 지켜라 3/keep-the-lead-3.js
@@ -16,28 +16,35 @@ for (let i = 0; i < m; i++) {
 }
 
 // 새배열 생성 및 시간에 따른 위치 정보 저장
-let posA = Array(1000).fill(0);
-let posB = Array(1000).fill(0);
+// [ 보완 ] 처음부터 시간 길이에 맞는 배열 제작 (기존 : 1000에서 뒷 부분을 자름)
+// let posA = Array(1000).fill(0);
+let posA = [0];
+// let posB = Array(1000).fill(0);
+let posB = [0];
 
 let timeA = 1;
 for (let i = 0; i < n; i++) {
   const [v, t] = A_moves[i];
   for (let j = 0; j < t; j++) {
-    posA[timeA] = posA[timeA - 1] + v;
-    timeA++
+    // posA[timeA] = posA[timeA - 1] + v;
+    timeA = timeA - 1 + v;
+    posA.push(timeA);
+    timeA++;
   }
 }
-posA = posA.slice(0, timeA);
+// posA = posA.slice(0, timeA);
 
 let timeB = 1;
 for (let i = 0; i < m; i++) {
   const [v, t] = B_moves[i];
   for (let j = 0; j < t; j++) {
-    posB[timeB] = posB[timeB - 1] + v;
-    timeB++;
+    // posB[timeB] = posB[timeB - 1] + v;
+    timeB = timeB - 1 + v;
+    posB.push(timeB);
+    timeB++;;
   }
 }
-posB = posB.slice(0, timeB);
+// posB = posB.slice(0, timeB);
 
 // 시간에 따른 선두 위치
 const totaltime = timeA; // timeA = timeB

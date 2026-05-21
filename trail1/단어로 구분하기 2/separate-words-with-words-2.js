2c48cf8490b4792b35a36e8dbb1878f8e8ef7725
변수 연 <xjkooy100@gmail.com>
2026-05-21 22:16:06 +0900

    [Passed] 단어로 구분하기 2 (66ms, 10MB)

--- trail1/단어로 구분하기 2/separate-words-with-words-2.js
+++ trail1/단어로 구분하기 2/separate-words-with-words-2.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(' ');
 
-for (let i = 0; i < input.length; i++) {
-    if (i % 2 === 0) console.log(input[i]);
+for (let i = 0; i < input.length; i += 2) {
+    console.log(input[i]);
 }

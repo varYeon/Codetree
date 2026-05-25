c8376fae7ea7c3d3fd7bd7319197755f7ac1988e
변수 연 <xjkooy100@gmail.com>
2026-05-25 22:00:28 +0900

    [Passed] Run Length 인코딩 (63ms, 10MB)

--- trail1/Run Length 인코딩/run-length-encoding.js
+++ trail1/Run Length 인코딩/run-length-encoding.js
@@ -5,6 +5,14 @@ const result = [];
 result.push(input[0]);
 let count = 1;
 
+// 예외 처리
+if (input.length === 1) {
+   const resultStr = input + "1";
+   console.log(resultStr.length);
+   console.log(resultStr);
+   return;
+}
+
 for (let i = 1; i < input.length; i++) {
 
    if (input[i - 1] === input[i]) {
@@ -21,6 +29,7 @@ for (let i = 1; i < input.length; i++) {
 }
 
 const resultStr = result.join('');
+// 문자열로 변환 후 길이 세기 (예외 처리 : count가 두 자리 수인 경우 배열의 길이를 세면 x)
 
 console.log(resultStr.length);
 console.log(resultStr);

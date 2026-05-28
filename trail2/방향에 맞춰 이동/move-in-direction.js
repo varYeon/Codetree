b6a968e66ef37be56d2fbec244e96456bb86d693
변수 연 <xjkooy100@gmail.com>
2026-05-28 17:32:44 +0900

    [Passed] 방향에 맞춰 이동 (63ms, 10MB)

--- trail2/방향에 맞춰 이동/move-in-direction.js
+++ trail2/방향에 맞춰 이동/move-in-direction.js
@@ -8,23 +8,19 @@ let x = 0, y = 0;
 let dx = [1, 0, -1, 0], dy = [0, -1, 0, 1];
 
 const dirMap = {
-  'E': 0,
-  'S': 1,
-  'W': 2,
-  'N': 3
+    'E': 0,
+    'S': 1,
+    'W': 2,
+    'N': 3
 };
 
 for (let i = 0; i < n; i++) {
-  let [direction, distance] = moves[i].split(' ');
-  distance = Number(distance);
-  direction = dirMap[direction];
+    let [direction, distance] = moves[i].split(' ');
+    distance = Number(distance);
+    direction = dirMap[direction];
 
-  let nx = x + (dx[direction] * distance);
-  let ny = y + (dy[direction] * distance);
-
-  // 누적
-  x = nx;
-  y = ny;
+    x += dx[direction] * distance;
+    y += dy[direction] * distance;
 }
 
 console.log(x, y);

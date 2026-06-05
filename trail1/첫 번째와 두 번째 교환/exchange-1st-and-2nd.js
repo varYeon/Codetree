52a31a9f25228d190171cd44a145f806ef6f556e
변수 연 <xjkooy100@gmail.com>
2026-06-05 15:40:25 +0900

    [Passed] 첫 번째와 두 번째 교환 (76ms, 15MB)

--- trail1/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
+++ trail1/첫 번째와 두 번째 교환/exchange-1st-and-2nd.js
@@ -1,12 +1,22 @@
+/**
+ * 문자열 -> 불변
+ * 1. 배열로 쪼개서 바꾸기
+ * 2. 새 문자열 만들어 넣기
+ */
+
+// 2.
 const fs = require("fs");
-let str = fs.readFileSync(0).toString().trim().split('');
+let str = fs.readFileSync(0).toString().trim();
 
 const first = str[0];
 const second = str[1];
 
+let result = '';
+
 for (let i = 0; i < str.length; i++) {
-    if (str[i] === first) str[i] = second;
-    else if (str[i] === second) str[i] = first;
+    if (str[i] === first) result += second;
+    else if (str[i] === second) result += first;
+    else result += str[i];
 }
 
-console.log(str.join(''));
+console.log(result);

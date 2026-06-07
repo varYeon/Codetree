e443deed2811030ff5f9a1e89440d52a2c129994
변수 연 <xjkooy100@gmail.com>
2026-06-07 12:13:58 +0900

    [Passed] 두 번째를 첫 번째로 (61ms, 10MB)

---
+++ trail1/두 번째를 첫 번째로/second-to-first.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let str = fs.readFileSync(0).toString().trim().split('');
+
+const first = str[0];
+const second = str[1];
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === second) str[i] = first;
+}
+
+console.log(str.join(''));

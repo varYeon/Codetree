bc2a2ef4eec3eae1570130fe6357e618a9c21271
변수 연 <xjkooy100@gmail.com>
2026-06-08 19:45:49 +0900

    [Passed] 양옆 문자 제거하기 (76ms, 10MB)

--- trail1/양옆 문자 제거하기/del-both-side-char.js
+++ trail1/양옆 문자 제거하기/del-both-side-char.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let str = fs.readFileSync(0).toString().trim().split('');
+const len = str.length;
 
-str[1] = '';
-str[str.length - 2] = '';
+const result = str.filter((_, idx) => idx !== 1 && idx !== len - 2);
 
-console.log(str.join(''));
+console.log(result.join(''));

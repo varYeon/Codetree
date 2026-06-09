545fa0c9b3990d26bab36b846d9b78c3ea42edfe
변수 연 <xjkooy100@gmail.com>
2026-06-09 11:57:16 +0900

    [Passed] 문자 제거하기 (67ms, 10MB)

--- trail1/문자 제거하기/del-char.js
+++ trail1/문자 제거하기/del-char.js
@@ -2,19 +2,15 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split('\n');
 
 let str = input[0].split('');
-// let newStr = str;
 
 for (let i = 1; i < input.length; i++) {
     if (str.length <= 1) break;
 
-    let curIdx = 1;
+    let curIdx = Number(input[i]);
 
-    if (input[i] > str.length) {
+    if (input[i] >= str.length) {
         curIdx = Number(str.length - 1);
-    }
-    else {
-        curIdx = Number(input[i]);
-    }
+    } 
 
     str = str.filter((_, idx) => idx !== curIdx);
 

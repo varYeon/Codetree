98239b279f940c6a20eaa33a7c545bacd9af1edb
변수 연 <xjkooy100@gmail.com>
2026-06-19 20:37:38 +0900

    [Passed] 빙빙 돌며 숫자 사각형 채우기 2 (65ms, 10MB)

---
+++ trail2/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+
+let result = Array.from(Array(n), () => Array(m).fill(0));
+
+const dx = [1, 0, -1, 0], dy = [0, 1, 0, -1];
+let x = 0, y = 0; // startPoint
+let dirNum = 0;
+result[x][y] = 1;
+
+function inRange(x, y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
+}
+
+for (let i = 2; i <= n * m; i++) {
+    let nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+    if (!inRange(nx, ny) || result[nx][ny] !== 0) {
+        dirNum = (dirNum + 1) % 4;
+    }
+
+    x += dx[dirNum];
+    y += dy[dirNum];
+    result[x][y] = i;
+}
+
+// 출력
+for (let i = 0; i < n; i++) {
+    let print = "";
+    for (let j = 0; j < m; j++) {
+        print += `${result[i][j]} `;
+    }
+
+    console.log(print);
+}

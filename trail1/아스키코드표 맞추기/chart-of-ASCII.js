d8f1b740278563f81d65a4e4a435663faf1332e1
변수 연 <xjkooy100@gmail.com>
2026-06-23 15:44:48 +0900

    [Passed] 아스키코드표 맞추기 (68ms, 10MB)

--- trail1/아스키코드표 맞추기/chart-of-ASCII.js
+++ trail1/아스키코드표 맞추기/chart-of-ASCII.js
@@ -7,6 +7,7 @@ for (let i = 0; i < input.length; i++) {
     // const num = Number(input[i]);
 
     result.push(String.fromCharCode(input[i]));
+    // String.fromCharCode() : 문자열 형태의 숫자도 내부적으로 자동으로 숫자로 형 변환(Type Coercion)
 }
 
 console.log(result.join(' '));

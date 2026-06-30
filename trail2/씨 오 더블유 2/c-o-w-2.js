63fe99d52b06e503285b7fadce637028b84ebe35
변수 연 <xjkooy100@gmail.com>
2026-06-30 12:05:40 +0900

    [Passed] 씨 오 더블유 2 (69ms, 10MB)

--- trail2/씨 오 더블유 2/c-o-w-2.js
+++ trail2/씨 오 더블유 2/c-o-w-2.js
@@ -5,13 +5,13 @@ const n = Number(input[0]);
 const string = input[1];
 
 let Ccount = 0;
-let Ocount = 0;
+let COcount = 0;
 let totalCount = 0;
 
 for (let i = 0; i < n; i++) {
     if (string[i] === 'C') Ccount++;
-    else if (Ccount && string[i] === 'O') Ocount++;
-    else if (Ocount && string[i] === 'W') totalCount += (Ccount * Ocount);
+    else if (Ccount && string[i] === 'O') COcount += Ccount;
+    else if (COcount && string[i] === 'W') totalCount += COcount;
 }
 
 console.log(totalCount);

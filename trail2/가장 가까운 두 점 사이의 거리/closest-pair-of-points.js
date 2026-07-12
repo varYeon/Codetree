5347112df2439d341edb7c107a7067b12de07f51
변수 연 <xjkooy100@gmail.com>
2026-07-12 12:58:08 +0900

    [Passed] 가장 가까운 두 점 사이의 거리 (68ms, 10MB)

--- trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.js
+++ trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.js
@@ -8,13 +8,12 @@ let minDis = Number.MAX_SAFE_INTEGER;
 
 for (let i = 0; i < n; i++) {
     const [x1, y1] = points[i];
-    let distance = Number.MAX_SAFE_INTEGER;
     for (let j = i + 1; j < n; j++) {
         const [x2, y2] = points[j];
 
-        distance = (x1 - x2) ** 2 + (y1 - y2) ** 2;
+        const distance = (x1 - x2) ** 2 + (y1 - y2) ** 2;
+        minDis = Math.min(minDis, distance);
     }
-    minDis = Math.min(minDis, distance);
 }
 
 console.log(minDis);

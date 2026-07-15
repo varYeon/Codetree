85d67a61b70cba3ef155bf412514abf587f57be3
변수 연 <xjkooy100@gmail.com>
2026-07-15 19:23:48 +0900

    [Wrong Answer] 스승의 은혜 2 (73ms, 10MB)

--- trail2/스승의 은혜 2/the-grace-form-teacher-2.js
+++ trail2/스승의 은혜 2/the-grace-form-teacher-2.js
@@ -1,26 +1,28 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-let [n, b] = input[0].split(' ').map(Number);
-const p = input.slice(1, 1 + n).map(Number);
+const [n, b] = input[0].split(' ').map(Number);
+const p = input.slice(1, 1 + n).map(Number).sort((a, b) => a - b);
 
-let maxCnt = 0;
+let maxStud = 0;
 
 for (let i = 0; i < n; i++) {
-    const newPrice = p[i] / 2;
-    b -= newPrice;
+    let totalPrice = b;
+    const halfPrice = p[i] / 2;
+    totalPrice -= halfPrice;
 
-    let cnt = 1; // i 구매 완료 => 1시작
+    if (totalPrice < 0) continue;
+    let students = 1; // i 구매 완료 => 1시작
 
     for (let j = 0; j < n; j++) {
         if (i === j) continue;
-        if (b <= 0) break;
+        if (totalPrice <= p[j]) break;
 
-        b -= p[j];
-        cnt += 1;
+        totalPrice -= p[j];
+        students += 1;
     }
 
-    maxCnt = Math.max(maxCnt, cnt);
+    maxStud = Math.max(maxStud, students);
 }
 
-console.log(maxCnt);
+console.log(maxStud);

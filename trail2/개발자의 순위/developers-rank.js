a413f92b3d0bf9f178ee4d5acdc40277cbb3cce3
변수 연 <xjkooy100@gmail.com>
2026-07-16 14:59:59 +0900

    [Passed] 개발자의 순위 (63ms, 10MB)

--- trail2/개발자의 순위/developers-rank.js
+++ trail2/개발자의 순위/developers-rank.js
@@ -3,23 +3,21 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [k, n] = input[0].split(' ').map(Number);
 const arr = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
-// [ [ 4, 1, 2, 3 ], [ 4, 1, 3, 2 ], [ 4, 2, 1, 3 ] ]
 
 let cnt = 0;
 
 // tip. 구하는 것에 대한 반복문 구조
-for (let a = 0; a < n; a++) {
-    for (let b = 0; b < n; b++) {
+for (let a = 1; a <= n; a++) {
+    for (let b = 1; b <= n; b++) {
         if (a === b) continue;
 
         let aWin = true;
         for (let i = 0; i < k; i++) {
-            // tip. indexof
+            // tip. indexOf()
             const aIdx = arr[i].indexOf(a);
             const bIdx = arr[i].indexOf(b);
 
-            if (aIdx < bIdx) aWin = true; // 숫자가 작아야 높은 등수
-            else {
+            if (aIdx >= bIdx) { // 숫자가 작아야 높은 등수
                 aWin = false;
                 break;
             }

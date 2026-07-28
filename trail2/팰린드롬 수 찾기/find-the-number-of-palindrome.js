a65c200b954c21c8e9edfad78f7de85a0cc616b6
변수 연 <xjkooy100@gmail.com>
2026-07-28 11:37:01 +0900

    [Passed] 팰린드롬 수 찾기 (84ms, 13MB)

--- trail2/팰린드롬 수 찾기/find-the-number-of-palindrome.js
+++ trail2/팰린드롬 수 찾기/find-the-number-of-palindrome.js
@@ -7,6 +7,11 @@ let cnt = 0;
 for (let i = x; i <= y; i++) {
     const num = i.toString().split('');
     let palindrome = true;
+    /**
+     * tip.
+     * 전체 조건 (전체가 다 맞아야 되는 경우) -> true 시작 (예외가 생기면 false)
+     * 부분 조건 (하나라도 맞으면 되는 경우) -> false 시작 (성공 사례 발견하면 true)
+     */
 
     for (let j = 0; j < num.length; j++) {
         if (num[j] !== num[num.length - 1 - j]) palindrome = false;

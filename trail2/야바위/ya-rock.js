f319830441677391f08df519978da54b40025bc8
변수 연 <xjkooy100@gmail.com>
2026-07-31 17:12:10 +0900

    [Passed] 야바위 (64ms, 11MB)

--- trail2/야바위/ya-rock.js
+++ trail2/야바위/ya-rock.js
@@ -4,14 +4,14 @@ const n = Number(input[0]);
 const commands = input.slice(1).map(line => line.split(' ').map(Number));
 
 let maxCnt = 0;
-const cups = [0, 0, 0];
+const cups = [0, 0, 0, 0];
 
-for (let i = 0; i < 3; i++) { // 조약돌 넣기
+for (let i = 1; i <= 3; i++) { // 조약돌 넣기
     cups[i] += 1;
     let cnt = 0;
 
     for (let j = 0; j < n; j++) {
-        const [a, b, c] = commands[j];
+        const [a, b, c] = commands[j]; 
         // a번과 b번을 교환 후 c번을 염
 
         const cupA = cups[a];
@@ -24,7 +24,7 @@ for (let i = 0; i < 3; i++) { // 조약돌 넣기
     maxCnt = Math.max(maxCnt, cnt);
 
     // 초기화
-    for (let k = 0; k < 3; k++) {
+    for (let k = 1; k <= 3; k++) {
         cups[k] = 0;
     }
 }

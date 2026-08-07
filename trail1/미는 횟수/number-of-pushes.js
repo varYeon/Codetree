d3b16815dac19584f20f52d7650006d66b59a8a3
변수 연 <xjkooy100@gmail.com>
2026-08-07 23:01:08 +0900

    [Passed] 미는 횟수 (82ms, 21MB)

--- trail1/미는 횟수/number-of-pushes.js
+++ trail1/미는 횟수/number-of-pushes.js
@@ -7,22 +7,19 @@ let n = 0;
 let same = false;
 
 for (let i = 0; i < strA.length; i++) {
-    // 옆으로 밀기, 카운트 세기
-    // 계속 strB와 같은지 파악, 같으면 그 떄 same = true
     let newA = '';
     const shift = i % strA.length;
+
+    newA = strA.slice(-shift) + strA.slice(0, -shift);
+    n++;
 
     if (newA === strB) {
         same = true;
         break;
     }
-
-    newA = strA.slice(-shift) + strA.slice(0, -shift);
-    console.log(newA)
-    n++;
-
 }
 
 if (!same) n = -1;
+else n -= 1
 
 console.log(n);

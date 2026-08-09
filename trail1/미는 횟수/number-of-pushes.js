48f87ea1e095464210ba560cff660bb072d4e66d
변수 연 <xjkooy100@gmail.com>
2026-08-09 12:27:59 +0900

    [Passed] 미는 횟수 (68ms, 15MB)

--- trail1/미는 횟수/number-of-pushes.js
+++ trail1/미는 횟수/number-of-pushes.js
@@ -10,7 +10,11 @@ for (let i = 0; i < strA.length; i++) {
     let newA = '';
     const shift = i % strA.length;
 
+    // length = 6
     newA = strA.slice(-shift) + strA.slice(0, -shift);
+    // (-shift) -> start = -shift, end = length(기본값) => -1 ~ 6, f
+    // (0, -shift) -> start = 0, end = length - shift (음수일 때) / (양수일 때) shift => 0 ~ 5, abcde (5 포함 x)
+    // ** end 값은 포함 X
     n++;
 
     if (newA === strB) {

d53c9b20d2bcc08f8b2acb87c9ab8aa7ea239fbb
변수 연 <xjkooy100@gmail.com>
2026-08-10 18:12:14 +0900

    [Passed] 독서실의 거리두기 5 (62ms, 10MB)

--- trail2/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
+++ trail2/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
@@ -6,14 +6,31 @@ const seat = input[1].split('');
 
 let distanceMax = 0;
 
+// 원래 인원 끼리 최소 거리 구하기
+let prevMin = 20;
+for (let i = 0; i < n - 1; i++) {
+    if (seat[i] !== '1') continue;
+
+    let distance = 20;
+    for (let j = i + 1; j < n; j++) {
+        if (seat[i] === '1' && seat[j] === '1') {
+            distance = Math.min(distance, Math.abs(i - j));
+        }
+    }
+    prevMin = Math.min(prevMin, distance);
+}
+
 for (let i = 0; i < n; i++) {
     if (seat[i] === '1') continue;
-    else seat[i] = '2';
+
+    seat[i] = '2';
 
     let diatance = 20;
     for (let j = 0; j < n; j++) {
-        if (seat[j] === '1')
-            diatance = Math.min(diatance, Math.abs(i - j))
+        // tip. 이미 앉은 사람과의 거리도 파악해야 함
+        if (seat[j] === '1') {
+            diatance = Math.min(diatance, Math.abs(i - j), prevMin)
+        }
     }
 
     seat[i] = '0';

2c158da295b0ea1a1f6ee481ca636fd496afed6e
변수 연 <xjkooy100@gmail.com>
2026-08-28 22:19:44 +0900

    [Wrong Answer] 두 직사각형 (64ms, 10MB)

--- trail2/두 직사각형/two-rectangles.js
+++ trail2/두 직사각형/two-rectangles.js
@@ -4,9 +4,8 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
 const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
 
-let isNotOverlap = true; // 겹치지 않음
+let isOverlap = true; // 겹침
 
-if(!(x2 <= a1 && b2 <= y1) || (a2 <= x1 && b1 <= y2)) isNotOverlap = false; // 겹침
+if ((x2 <= a1) || (a2 <= x1) || (y2 <= b1) || (b2 <= y1)) isOverlap = false; // 겹치지 않음
 
-if (!isNotOverlap) console.log('overlapping');
-else console.log('nonoverlapping');
+console.log(isOverlap ? 'overlapping' : 'nonoverlapping');

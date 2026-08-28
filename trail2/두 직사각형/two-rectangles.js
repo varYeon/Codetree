270a576775fbf87df8a2f3d8d332aba651a26985
변수 연 <xjkooy100@gmail.com>
2026-08-28 22:20:51 +0900

    [Passed] 두 직사각형 (64ms, 10MB)

--- trail2/두 직사각형/two-rectangles.js
+++ trail2/두 직사각형/two-rectangles.js
@@ -6,6 +6,6 @@ const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
 
 let isOverlap = true; // 겹침
 
-if ((x2 <= a1) || (a2 <= x1) || (y2 <= b1) || (b2 <= y1)) isOverlap = false; // 겹치지 않음
+if ((x2 < a1) || (a2 < x1) || (y2 < b1) || (b2 < y1)) isOverlap = false; // 겹치지 않음
 
 console.log(isOverlap ? 'overlapping' : 'nonoverlapping');

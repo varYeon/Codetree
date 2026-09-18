66ed27d84f368efaefba21a9d3e6520888cf54f4
변수 연 <xjkooy100@gmail.com>
2026-09-19 00:16:01 +0900

    [Passed] 3 또는 5의 배수 (76ms, 14MB)

---
+++ trail1/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a % 3 === 0) console.log('YES');
+else console.log('NO');
+
+if (a % 5 === 0) console.log('YES');
+else console.log('NO');
